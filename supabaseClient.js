133f8dd2e6b9dbf8c5ecaaae28b45caecca63cde
Stephen Kortum <stephenkortum@yahoo.com>
2025-07-03 21:15:32 -0400

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,13 +1,9 @@
 // supabaseClient.js
 import {createClient} from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
     const supabaseUrl = 'https://uxytxzliqxpinajccvbu.supabase.co';
     const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4eXR4emxpcXhwaW5hamNjdmJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1Nzg4MjgsImV4cCI6MjA2NDE1NDgyOH0.1RE6lzGdoj7kfaExaJrBUap9TwuQs5d3TxLarQaweb8';
     const supabase = createClient(supabaseUrl, supabaseKey, {
-        auth: {
-                storage: localStorage,
-                persistSession: true,
-                autoRefreshToken: true,
-                detectSessionInUrl: false
-            }
-    });
+
+});
+
+window.supabase = supabase; // Make supabase globally accessible
